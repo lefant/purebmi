cac3d6954cdcfba696d462ef994725a3f802f43a
Fabian Linzberger <e@lefant.net>
2015-03-01 11:37:11 +0100

    working basic bmi calculation

--- src/Main.purs
+++ src/Main.purs
@@ -2,6 +2,7 @@ module Main (main) where
 
 import Data.Function
 import Data.Maybe
+import Math
 
 import qualified Thermite as T
 import qualified Thermite.Html as T
@@ -11,21 +12,21 @@ import qualified Thermite.Action as T
 import qualified Thermite.Events as T
 import qualified Thermite.Types as T
 
-data Action = Increment | Decrement | Input Number
+data Action = UpdateHeight Number | UpdateMass Number
 
-type State = { counter :: Number
+type State = { height :: Number
              , mass :: Number
-             , height :: Number
              }
 
 initialState :: State
-initialState = { counter: 0
+initialState = { height: 175
                , mass: 75
-               , height: 175
                }
 
 bmi :: Number -> Number -> Number
-bmi mass height = mass / (height * height)
+bmi heightCm mass = mass / (heightM `pow` 2)
+  where
+    heightM = heightCm / 100
 
 foreign import getValue
   "function getValue(e) {\
@@ -33,49 +34,54 @@ foreign import getValue
   \}" :: T.FormEvent -> Number
 
 render :: T.Render State _ Action
-render ctx s _ = T.div' [counter, textInput, slider, buttons]
+render ctx st _ =
+  T.div'
+  [ result
+  , textAndSlider "Height (in cm)" (\st -> st.height) (\v -> UpdateHeight v) 10 250
+  , textAndSlider "Mass (in kg)" (\st -> st.mass) (\v -> UpdateMass v) 1 300
+  ]
   where
-  counter :: T.Html _
-  counter = 
+  result :: T.Html _
+  result =
     T.p'
-      [ T.text "Value: "
-      , T.text $ show s.counter
+      [ T.text " BMI: "
+      , T.text $ show $ round $ bmi st.height st.mass
       ]
 
-  slider :: T.Html _
-  slider =
+  textAndSlider :: String -> (State -> Number) -> (Number -> Action) -> Number -> Number -> T.Html _
+  textAndSlider name value updateAction sMin sMax =
+    T.p'
+    [ T.text name
+    , slider value updateAction sMin sMax
+    , textInput value updateAction
+    ]
+
+  slider :: (State -> Number) -> (Number -> Action) -> Number -> Number -> T.Html _
+  slider value updateAction sMin sMax =
     T.p'
       [ T.input
         [ A._type "range"
-        , T.onChange ctx (\e -> Input (getValue e)) ]
-        [ T.text "Range" ]
+        , A.value (show $ value st)
+        , A.min (show sMin)
+        , A.max (show sMax)
+        , T.onChange ctx (\e -> updateAction (getValue e))
+        ] []
       ]
 
-  textInput :: T.Html _
-  textInput =
+  textInput :: (State -> Number) -> (Number -> Action) -> T.Html _
+  textInput value updateAction =
     T.p'
-      [ T.input [ A.value (show s.counter)
-                , T.onChange ctx (\e -> Input (getValue e))
+      [ T.input [ A.value (show $ value st)
+                , T.onChange ctx (\e -> updateAction (getValue e))
                 ] []
       ]
 
-  buttons :: T.Html _
-  buttons = 
-    T.p'
-      [ T.button [ T.onClick ctx (\_ -> Increment) ] 
-                 [ T.text "Increment" ]
-      , T.button [ T.onClick ctx (\_ -> Decrement) ] 
-                 [ T.text "Decrement" ]
-      ]
-
-performAction :: T.PerformAction _ Action (T.Action _ State) 
-performAction _ Increment = T.modifyState \o -> o { counter = o.counter + 1 }
-performAction _ Decrement = T.modifyState \o -> o { counter = o.counter - 1 }
-performAction _ (Input s) = T.modifyState \o -> o { counter = s }
+performAction :: T.PerformAction _ Action (T.Action _ State)
+performAction _ (UpdateHeight n) = T.modifyState \o -> o { height = n }
+performAction _ (UpdateMass n) = T.modifyState \o -> o { mass = n }
 
 spec :: T.Spec _ State _ Action
 spec = T.simpleSpec initialState performAction render
-         # T.componentWillMount Increment
 
 main = do
   let component = T.createClass spec
