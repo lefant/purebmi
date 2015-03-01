e693f372a985ec2a39462bfde859e60b04830a60
Fabian Linzberger <e@lefant.net>
2015-03-01 17:26:44 +0100

    s/Mass/Weight/

--- src/Main.purs
+++ src/Main.purs
@@ -40,7 +40,7 @@ render ctx st _ =
        T.div [ A.className "col-xs-12 col-sm-3 col-md-2 col-lg-2" ] [
           T.div'
           [ textAndSlider "Height (in cm):" (\st -> st.height) (\v -> UpdateHeight v) 130 220
-          , textAndSlider "Mass (in kg):" (\st -> st.mass) (\v -> UpdateMass v) 25 150
+          , textAndSlider "Weight (in kg):" (\st -> st.mass) (\v -> UpdateMass v) 25 150
           , result
           ]]]]
   where
